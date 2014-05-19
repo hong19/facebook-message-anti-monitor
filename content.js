4b25f92cf3bb8eb54e9288da298770db3d837777
honghong <jimmy600kimo@yahoo.com.tw>
2014-05-19 23:32:59 +0800

    bind DOMSubtreeModified
    not work, don't know the correct way to pass the parameters to the event
    handler

--- content.js
+++ content.js
@@ -7,9 +7,38 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 			
 			var targetName = message.target;
 			
-			$("a.titlebarText:contains('" + targetName + "')").html("Anonymous");
-			$("[aria-label$='" + targetName + "'] img").attr( "src","https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/t1.0-1/p40x40/1891065_10203713186696473_1228623972_t.jpg" );
-			
+
+			//after document-load
+			$("a.titlebarText:contains('" + targetName + "')").bind('DOMSubtreeModified', test( event , targetName ) );
+			//$("a.titlebarText:contains('" + targetName + "')").attr( "aria-level" , "2");
+			replaceTheName( targetName );
+		
 		break;
 	}
 });
+
+function replaceTheName( targetName ){
+	console.log("replace " + targetName );
+	$("a.titlebarText:contains('" + targetName + "')").html("Anonymous");
+	$("[aria-label$='" + targetName + "'] img").attr( "src","https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/t1.0-1/p40x40/1891065_10203713186696473_1228623972_t.jpg" );
+}
+
+function DOMModificationHandler( event ){
+	$(this).unbind();
+	 
+	 //var message = "The value of the " + eventObj.attrName + " attribute has been changed from " + eventObj.prevValue + " to " + eventObj.newValue + ".";
+	//console.log( message );
+	
+	setTimeout(function(){
+		replaceTheName( event.data.targetName );
+		$(this).bind('DOMAttrModified', { "targetName": targetName } ,DOMModificationHandler() );
+	},1000);
+}
+
+
+
+function test ( event , targetName ){
+	console.log( "test event trigger" );
+	console.log( "target: " +  targetName );
+	console.log( event );			
+}
